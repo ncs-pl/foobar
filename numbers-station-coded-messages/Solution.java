9e98b6957545155a542b5bf2c47f128c470d7482
n1c00o <git.n1c00o@gmail.com>
2021-10-06 18:19:50 +0200

    add challenge numbers-station-coded-messages

---
+++ numbers-station-coded-messages/Solution.java
@@ -0,0 +1,5 @@
+public​ ​class​ ​Solution​ ​{
+​ ​​ ​​ ​​ ​public​ ​static​ ​int[]​ ​solution(int[]​ ​l,​ ​int​ ​t)​ ​{
+​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​//​ ​Your​ ​code​ ​here
+​ ​​ ​​ ​​ ​}
+}
---
+++ numbers-station-coded-messages/solution.py
@@ -0,0 +1,2 @@
+def​ ​solution(l,​ ​t):
+​ ​​ ​​ ​​ ​#​ ​Your​ ​code​ ​here
