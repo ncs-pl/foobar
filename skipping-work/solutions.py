af0d281cbe4691994e2242e1ed9da881b46f7f6d
n1c00o <git.n1c00o@gmail.com>
2021-10-06 18:16:48 +0200

    Add result for skipping-work

---
+++ skipping-work/solutions.py
@@ -0,0 +1,10 @@
+def​ ​solution(x,​ ​y):
+​ ​​ ​​ ​​ ​# x and y are two list of integers
+    for el in x:
+      if not el in y:
+        return el
+    for el in y:
+      if not el in x:
+        return el
+
+    
