41349211cbd067492938eab837b6e6ce08f758ae
n1c00o <git.n1c00o@gmail.com>
2021-10-06 19:04:46 +0200

    Add solution for numbers-station-coded-messages

--- numbers-station-coded-messages/solution.py
+++ numbers-station-coded-messages/solution.py
@@ -1,18 +1,11 @@
 def solution(l, t):
     for start_i in range(len(l)):
-        sum = l[start_i]
+        sum = 0
 
-        for curr_i in range(start_i + 1, len(l), 1):
+        for curr_i in range(start_i, len(l)):
             sum += l[curr_i]
 
             if sum == t:
                 return [start_i, curr_i]
-            elif sum > t:
-                break
 
     return [-1, -1]
-
-
-# tests
-assert solution([1, 2, 3, 4], 15) == [-1, -1], "Test 1 failed"
-assert solution([4, 3, 10, 2, 8], 12) == [2, 3], "Test 2 failed"
