9fc3ad046be744e565497983b7832132d4723f3f
n1c00o <git.n1c00o@gmail.com>
2021-10-15 20:36:03 +0200

    disorderly-escape

---
+++ disorderly-escape/Solution.java
@@ -0,0 +1,5 @@
+public​ ​class​ ​Solution​ ​{
+​ ​​ ​​ ​​ ​public​ ​static​ ​String​ ​solution(int​ ​w,​ ​int​ ​h,​ ​int​ ​s)​ ​{
+​ ​​ ​​ ​​ ​​ ​​ ​​ ​​ ​//​ ​Your​ ​code​ ​here
+​ ​​ ​​ ​​ ​}
+}
---
+++ disorderly-escape/n1c00o/solution.py
@@ -0,0 +1,2 @@
+def solution(w, h, s):
+  # Your code here
